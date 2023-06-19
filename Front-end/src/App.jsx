bb1ee3f65fa051dc509463969610edc490b1aa8e
Letícia Santos <leticiavsantoos@gmail.com>
2023-06-19 14:48:46 -0300

    style: tweaks to the style of the login page and "know more" page

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import './App.css';
 import SaibaMais from './SaibaMais';
+import './App.css';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -11,25 +11,30 @@ function App() {
   };
 
   return (
-    <>
-      {!loggedIn ? (
-        <section className='Conteiner-Login'>
-          <h2 className='h2-Login'>Login</h2>
-          <form className='Form-Login'>
-            <div className='Conteiner-Inputs'>
-              <input type="email" placeholder='e-mail' className='Input-Login'/>
-              <input type="password" placeholder='senha' className='Input-Login'/>
+    <div className='Container'>
+      {/* O conteúdo é renderizado se o usuário não estiver logado */}
+        {!loggedIn ? (
+          <section className='Container-Body'>
+            <div className='Container-Login'>
+            <h2 className='h2-Login'>Login</h2>
+              <form className='Form-Login'>
+                <div className='Container-Inputs'>
+                  <input type="email" placeholder='e-mail' className='Input-Email'/>
+                  <input type="password" placeholder='senha' className='Input-Senha'/>
+                </div>
+                <div className='Button-Ancora'>
+                  <button onClick={login} className='Button-Login'>Entrar</button>
+                  <a href='' className='Ancora-CriarConta'>Criar Conta</a>
+                </div>
+              </form>
             </div>
-            <div className='Conteiner-Buttons'>
-              <button onClick={login} className='Button-Login'>Login</button>
-              <button className='Button-CriarConta'>Criar Conta</button>
-            </div>
-          </form>
-        </section>
-      ) : (
-        <SaibaMais />
-      )}
-    </>
+          </section>
+        ) : (
+          // Se o usuário estiver logado para de renderizar o componente App e passa a renderizar o componente SaibaMais
+          <SaibaMais />
+        )
+      }
+    </div>
   );
 }
 
--- Front-end/src/SaibaMais.jsx
+++ Front-end/src/SaibaMais.jsx
@@ -7,71 +7,68 @@ import './SaibaMais.css'
 
 export default function SaibaMais(){
     return(
-        <>
-            <div className='SaibaMais-Habits'>
-                <section className='Sobre-Habits'>
-                    <h1 className='h1-SaibaMais'>Sobre o Habits</h1>
-                    <div className='Conteiner-Sobre'>
-                        <figure className='Logo-Habits'>
-                            <img src={logo} alt="" className='Logo-Habits-Img'/>
-                        </figure>
-                        <p className='Descricao-Habits'>O Gerenciador de hábitor foi selecionado para ser desenvolvido pelo grupo porque Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium in animi recusandae incidunt dolorem quidem hic, praesentium ipsam fugit qui quibusdam, maxime facilis autem eveniet et nesciunt tenetur officia. Error. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, molestias? Cumque, voluptas natus! Illum labore nostrum unde fuga dicta perferendis ea aspernatur ipsa et, esse eius maxime porro aut totam.
-                        </p>
+        <div className='Container-Main'>
+            <section className='Sobre-Habits'>
+                <h1 className='h1-SaibaMais'>Sobre o Habits</h1>
+                <div className='Conteiner-Sobre'>
+                    <figure className='Logo-Habits'>
+                        <img src={logo} alt="" className='Logo-Habits-Img'/>
+                    </figure>
+                    <p className='Descricao-Habits'>O Gerenciador de hábitor foi selecionado para ser desenvolvido pelo grupo porque Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium in animi recusandae incidunt dolorem quidem hic, praesentium ipsam fugit qui quibusdam, maxime facilis autem eveniet et nesciunt tenetur officia. Error. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, molestias? Cumque, voluptas natus! Illum labore nostrum unde fuga dicta perferendis ea aspernatur ipsa et, esse eius maxime porro aut totam.
+                    </p>
+                </div>
+                
+            </section>
+
+            <section className='Devs-Habits'>
+                <h2 className='h2-Devs'>Desenvolvedores</h2>
+                <p className='Descricao-Geral'>Os integrantes desse grupo são graduandos da <a href="https://unifei.edu.br/" className='Link-Unifei'>Universasidade Federal de Itajubá (UNIFEI)</a>, sendo, expecificamente alunos do <a href="https://imc.unifei.edu.br/" className='Link-Imc'>Instituto de Matemática e Computação (IMC)</a>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam repellendus blanditiis esse nisi dolor illum aliquid voluptatem, mollitia nam? Quos veniam accusantium aut expedita iure, inventore tempora ipsam architecto possimus.</p>
+                <section className='Devs-Descricao'>
+                    <div className='Devs-Descricao1'>
+                        <div className='Dev-Ana'>
+                            <figure>
+                                <img src={fotoAna} alt="" className='Devs-Foto'/>
+                            </figure>
+                            <h3 className='Devs-Nome'>Ana Maisa</h3>
+                            <p className='Devs-graduacao'>Graduanda de Ciências da Computação</p>
+                            <a href="" className='Devs-GitHub'>GitHub</a>
+                            <a href="" className='Devs-Linkedin'>Linkedin</a>
+                        </div>
+                        <div className='Dev-Daniel'>
+                            <figure>
+                                <img src={fotoDaniel} alt="" className='Devs-Foto'/>
+                            </figure>
+                            <h3 className='Devs-Nome'>Daniel Gonzalez</h3>
+                            <p className='Devs-graduacao'>Graduando de Sistemas de Informação</p>
+                            <a href="https://github.com/danigon1998" className='Devs-GitHub'>GitHub</a>
+                            <a href="linkedin.com/in/daniel-gonzález-a5662b181" className='Devs-Linkedin'>Linkedin</a>
+                        </div>
+                    </div>
+
+                    <div className='Devs-Descricao2'>
+                        <div className='Dev-Huandy'>
+                            <figure>
+                                <img src={fotoHuandy} alt="" className='Devs-Foto'/>
+                            </figure>
+                            <h3 className='Devs-Nome'>Huandy Calini</h3>
+                            <p className='Devs-graduacao'>Graduanda de Sistemas de Informação</p>
+                            <a href="https://github.com/HuandySilva" className='Devs-GitHub'>GitHub</a>
+                            <a href="https://br.linkedin.com/in/huandy-silva" className='Devs-Linkedin'>Linkedin</a>
+                        </div>
+                        <div className='Dev-Leticia'>
+                            <figure>
+                                <img src={fotoLeticia} alt="" className='Devs-Foto'/>
+                            </figure>
+                            <h3 className='Devs-Nome'>Letícia Santos</h3>
+                            <p className='Devs-graduacao'>Graduanda de Sistemas de Informação</p>
+                            <a href="https://github.com/leticiasants" className='Devs-GitHub'>GitHub</a>
+                            <a href="https://br.linkedin.com/in/leticiasantss" className='Devs-Linkedin'>Linkedin</a>
+                        </div>
                     </div>
                     
+                    
                 </section>
-
-                <section className='Devs-Habits'>
-                    <h2 className='h2-Devs'>Desenvolvedores</h2>
-                    <p className='Descricao-Geral'>Os integrantes desse grupo são graduandos da <a href="https://unifei.edu.br/" className='Link-Unifei'>Universasidade Federal de Itajubá (UNIFEI)</a>, sendo, expecificamente alunos do <a href="https://imc.unifei.edu.br/" className='Link-Imc'>Instituto de Matemática e Computação (IMC). </a></p>
-                    <section className='Devs-Descricao'>
-                        <div className='Devs-Descricao1'>
-                            <div className='Dev-Ana'>
-                                <figure>
-                                    <img src={fotoAna} alt="" className='Devs-Foto'/>
-                                </figure>
-                                <h3 className='Devs-Nome'>Ana Maisa</h3>
-                                <p className='Devs-graduacao'>Graduanda de Ciências da Computação</p>
-                                <a href="" className='Devs-GitHub'>GitHub</a>
-                                <a href="" className='Devs-Linkedin'>Linkedin</a>
-                            </div>
-                            <div className='Dev-Daniel'>
-                                <figure>
-                                    <img src={fotoDaniel} alt="" className='Devs-Foto'/>
-                                </figure>
-                                <h3 className='Devs-Nome'>Daniel Gonzalez</h3>
-                                <p className='Devs-graduacao'>Graduando de Sistemas de Informação</p>
-                                <a href="https://github.com/danigon1998" className='Devs-GitHub'>GitHub</a>
-                                <a href="linkedin.com/in/daniel-gonzález-a5662b181" className='Devs-Linkedin'>Linkedin</a>
-                            </div>
-                        </div>
-
-                        <div className='Devs-Descricao2'>
-                            <div className='Dev-Huandy'>
-                                <figure>
-                                    <img src={fotoHuandy} alt="" className='Devs-Foto'/>
-                                </figure>
-                                <h3 className='Devs-Nome'>Huandy Calini</h3>
-                                <p className='Devs-graduacao'>Graduanda de Sistemas de Informação</p>
-                                <a href="https://github.com/HuandySilva" className='Devs-GitHub'>GitHub</a>
-                                <a href="https://br.linkedin.com/in/huandy-silva" className='Devs-Linkedin'>Linkedin</a>
-                            </div>
-                            <div className='Dev-Leticia'>
-                                <figure>
-                                    <img src={fotoLeticia} alt="" className='Devs-Foto'/>
-                                </figure>
-                                <h3 className='Devs-Nome'>Letícia Santos</h3>
-                                <p className='Devs-graduacao'>Graduanda de Sistemas de Informação</p>
-                                <a href="https://github.com/leticiasants" className='Devs-GitHub'>GitHub</a>
-                                <a href="https://br.linkedin.com/in/leticiasantss" className='Devs-Linkedin'>Linkedin</a>
-                            </div>
-                        </div>
-                        
-                        
-                    </section>
-                </section>
-            </div>
-        
-        </>
+            </section>
+        </div>
     )
 }
