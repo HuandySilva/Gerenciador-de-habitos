7121e01dba5f35ffd3d9ff3a5d81e1135c3d5ab1
Letícia Santos <leticiavsantoos@gmail.com>
2023-06-18 22:07:44 -0300

    feat: login page and "know more" page

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import SaibaMais from './SaibaMais';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const login = (event) => {
+    event.preventDefault();
+    setLoggedIn(true);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!loggedIn ? (
+        <section className='Conteiner-Login'>
+          <h2 className='h2-Login'>Login</h2>
+          <form className='Form-Login'>
+            <div className='Conteiner-Inputs'>
+              <input type="email" placeholder='e-mail' className='Input-Login'/>
+              <input type="password" placeholder='senha' className='Input-Login'/>
+            </div>
+            <div className='Conteiner-Buttons'>
+              <button onClick={login} className='Button-Login'>Login</button>
+              <button className='Button-CriarConta'>Criar Conta</button>
+            </div>
+          </form>
+        </section>
+      ) : (
+        <SaibaMais />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
