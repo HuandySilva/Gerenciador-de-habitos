57215a5ee410565cbe9075f2168e46b74d36312f
Huandy <huandysilva@gmail.com>
2023-07-14 14:56:07 -0300

    test

--- back-end/src/main/java/com/unifei/gerenciadorhabitos/SpringConfig/WebSecurityConfig.java
+++ back-end/src/main/java/com/unifei/gerenciadorhabitos/SpringConfig/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.unifei.gerenciadorhabitos.SpringConfig;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +21,7 @@ public class WebSecurityConfig {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 http
-                                .csrf(csrf -> csrf.disable()) // mudar mais tarde
+                                // .csrf(csrf -> csrf.disable()) // mudar mais tarde
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers("/users/**").permitAll()
                                                 .anyRequest().authenticated())
@@ -30,4 +35,13 @@ public class WebSecurityConfig {
                 return new BCryptPasswordEncoder();
         }
 
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 }
